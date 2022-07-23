def97775d68c43bbdf12a44ec107c2789441daf5
Scof <32575460+songyw0517@users.noreply.github.com>
2022-07-24 05:16:47 +0900

    create response format, class, interface

--- server/flask/app/api/__init__.py
+++ server/flask/app/api/__init__.py
@@ -1,7 +1,7 @@
 """
 API Request Handler and util
 """
-from flask import jsonify
+from abc import ABCMeta, abstractmethod
 
 
 def init_app(app):
@@ -33,3 +33,81 @@ def init_app(app):
     def teardown_appcontext(exception):
         print("4. HTTP요청의 어플리케이션 컨텍스트가 종료될때 실행")
         print("*"*30)
+
+
+"""
+Response metaclass (interface)
+"""
+class Response_metaclass(metaclass=ABCMeta):
+    # 200~
+    @abstractmethod
+    def response_ok(result=None):
+        pass
+
+    @abstractmethod
+    def response_no_content(result=None):
+        pass
+    @abstractmethod
+    def response_partial_content(result=None):
+        pass
+
+    
+    # 300~ : Redirection
+    @abstractmethod
+    def response_moved_permanently(description=None):
+        pass
+
+    @abstractmethod
+    def response_found(description=None):
+        pass
+
+    @abstractmethod
+    def response_see_other(description=None):
+        pass
+
+    @abstractmethod
+    def response_not_modified(description=None):
+        pass
+
+    @abstractmethod
+    def response_temporary_redirect(description=None):
+        pass
+
+
+    # 400~ : Client Error
+    @abstractmethod
+    def response_bad_request(description=None):
+        pass
+    
+    @abstractmethod
+    def response_unauthorized(description=None):
+        pass
+
+    @abstractmethod
+    def response_forbidden(description=None):
+        pass
+
+    @abstractmethod
+    def response_not_found(description=None):
+        pass
+
+    @abstractmethod
+    def response_method_now_allowed(description=None):
+        pass
+
+    # 500~ : Server Error
+    @abstractmethod
+    def response_internal_server_error(description=None):
+        pass
+    
+    @abstractmethod
+    def response_service_unavailable(description=None):
+        pass
+
+    @abstractmethod
+    def response_gateway_timeout(description=None):
+        pass
+
+    @abstractmethod
+    def response_http_version_not_supported(description=None):
+        pass
--- server/flask/app/api/response.py
+++ server/flask/app/api/response.py
@@ -1,35 +1,137 @@
 """Response Shortcuts"""
 """ Response 정의 """
-def response_200(result=None):
-    if result is None:
-        return {'msg': 'success'}, 200
-    else:
-        return {'msg': 'success', 'result': result}, 200
+from app.api import Response_metaclass
+class Response(Response_metaclass):
+    # 200~ : Success response
+    @staticmethod
+    def response_ok(result=None):
+        return {
+            'msg':'success',
+            'status_code':200,
+            'status_msg':'OK',
+            'result': result
+        }, 200
 
-created = ({"msg": "created"}, 201)
+    @staticmethod
+    def response_no_content(result=None):
+        return {
+            'msg':'success',
+            'status_code':204,
+            'status_msg':'No Content',
+            'result': result
+        }, 204
+    
+    @staticmethod
+    def response_partial_content(result=None):
+        return {
+            'msg':'success',
+            'status_code':206,
+            'status_msg':'Partial Content',
+            'result': result
+        }, 206
 
-no_content = ({}, 204)
+    
+    # 300~ : Redirection
+    @staticmethod
+    def response_moved_permanently(description=None):
+        pass
+
+    @staticmethod
+    def response_found(description=None):
+        pass
+
+    @staticmethod
+    def response_see_other(description=None):
+        pass
+
+    @staticmethod
+    def response_not_modified(description=None):
+        pass
+
+    @staticmethod
+    def response_temporary_redirect(description=None):
+        pass
+
+    # 400~ : Client Error
+    @staticmethod
+    def response_bad_request(description=None):
+        return {
+            'msg': 'fail',
+            'status_code':400,
+            'status_msg':'Bad Request',
+            'description': description
+        }, 400
+    
+    @staticmethod
+    def response_unauthorized(description=None):
+        return {
+            'msg': 'fail',
+            'status_code':401,
+            'status_msg':'Unauthorized',
+            'description': description
+        }, 401
 
 
-def bad_request(description):
-    return {'msg': 'fail', 'description': description}, 400
+    @staticmethod
+    def response_forbidden(description=None):
+        return {
+            'msg': 'fail',
+            'status_code':403,
+            'status_msg':'Forbidden',
+            'description': description
+        }, 403
 
+    @staticmethod
+    def response_not_found(description=None):
+        return {
+            'msg': 'fail',
+            'status_code':404,
+            'status_msg':'Not Found',
+            'description': description
+        }, 404
 
-bad_access_token = (
-    {
-        'msg':'fail',
-        'description': "Bad access token"
-    }, 401
-)
+    @staticmethod
+    def response_method_now_allowed(description=None):
+        return {
+            'msg': 'fail',
+            'status_code':405,
+            'status_msg':'Mothod Not Allowed',
+            'description': description
+        }, 405
 
-def forbidden(description):
-    return {
-        'msg': 'fail',
-        'description': description
-    }, 403
-
-
-not_found = {
-    'msg': 'fail',
-    'description': "Resource not found."
-}, 404
+    # 500~ : Server Error
+    @staticmethod
+    def response_internal_server_error(description=None):
+        return {
+            'msg': 'fail',
+            'status_code':500,
+            'status_msg':'Internal Server Error',
+            'description': description
+        }, 500
+    
+    @staticmethod
+    def response_service_unavailable(description=None):
+        return {
+            'msg': 'fail',
+            'status_code':503,
+            'status_msg':'Service Unavailable',
+            'description': description
+        }, 503
+    
+    @staticmethod
+    def response_gateway_timeout(description=None):
+        return {
+            'msg': 'fail',
+            'status_code':504,
+            'status_msg':'Gateway Timeout',
+            'description': description
+        }, 504
+    
+    @staticmethod
+    def response_http_version_not_supported(description=None):
+        return {
+            'msg': 'fail',
+            'status_code':505,
+            'status_msg':'HTTP Version Not Supported',
+            'description': description
+        }, 505
