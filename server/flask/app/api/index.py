f8a4abb900c3dfa6cbb755c1e44d0b06b3cc9473
Scof <32575460+songyw0517@users.noreply.github.com>
2022-07-24 05:17:32 +0900

    response test api

--- server/flask/app/api/index.py
+++ server/flask/app/api/index.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
+
+from app.api.response import Response
+
 class index:
     bp = Blueprint('index', __name__)
 
     @bp.route('/')
     def index():
         return render_template('index.html')
+
+    @bp.route('/response_test')
+    def res_test():
+        return Response.response_200()
