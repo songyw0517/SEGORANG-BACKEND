cea2ae0a9bfe6da2ccb294aff9fef870168691f9
Scof <32575460+songyw0517@users.noreply.github.com>
2022-07-24 05:17:37 +0900

    Update error_handler.py

--- server/flask/app/api/error_handler.py
+++ server/flask/app/api/error_handler.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from werkzeug.exceptions import HTTPException
-from app.api.response import not_found, bad_request
+from app.api.response import Response
 
 class error_handler:
     bp = Blueprint("error_handler", __name__)
@@ -8,16 +8,16 @@ class error_handler:
     @bp.app_errorhandler(400)
     def bad_request_error(error: HTTPException):
         """400 Error Handler"""
-        return bad_request(error.description)
+        return Response.response_bad_request(error.description)
 
 
     @bp.app_errorhandler(404)
     def not_found_error(error: HTTPException):
         """404 Error Handler"""
-        return not_found
+        return Response.response_not_found(error.description)
 
 
     @bp.app_errorhandler(500)
     def internal_server_error(error):
         """500 Error Handler (production only)"""
-        return jsonify(msg=str(error)), 500
+        return Response.response_internal_server_error(error)
